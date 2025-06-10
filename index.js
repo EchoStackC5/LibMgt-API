c95847938e5a7032ad66be97d07eb5ea010d5900
kaagyebi <kaagyebi@gmail.com>
2025-06-10 14:43:45 -0700

    added cors

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import express from "express"
 import {bookRouter} from "./routes/book.routes.js"
 import mongoose from "mongoose"
 import 'dotenv/config'
+import cors from cors
 
 const app = express()
 
 const PORT = process.env.PORT || 8085
 
 app.use(express.json());
-app.use(bookRouter);
+app.use(cors())
+
+
+app.use("/api/v1", bookRouter);
 
 const mongo_URI = process.env.MONGO_URI
 await mongoose.connect(mongo_URI);
